240144aa67fca99b0a049222eb8fce8af80e94f4
EC2 Default User <ec2-user@ip-172-31-6-34.ap-northeast-1.compute.internal>
2023-02-11 11:18:33 +0000

    [add]index/show/edit.view [finish]update/destroy機能 flashメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    @books=Book.all
+    if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render:index
+    end
   end
 
   def show
@@ -19,12 +24,20 @@ class BooksController < ApplicationController
 
   def edit
     @book=Book.find(params[:id])
-    
+
   end
-  
+
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice]="Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
+    flash[:notice]="Book was successfully destroyed."
     redirect_to'/books'
   end
 

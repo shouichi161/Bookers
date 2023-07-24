e028b318fc3681635bddd0943803d4fac6e923c1
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-07-24 12:10:45 +0000

    [add]config/schedule.rbの記述をwheneverでcrontabに反映させる

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,3 +18,16 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+rails_env = Rails.env.to_sym
+set :environment, rails_env
+set :output, 'log/cron.log'
+every 2.minute do
+  begin
+    runner "Batch::DataReset.data_reset"
+  rescue => e
+    Rails.logger.error("aborted rails runner")
+    raise e
+  end
+end
